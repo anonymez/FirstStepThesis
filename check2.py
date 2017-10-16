67a15a3373f821ae288b328a5b4fcc97b140221a
thesisInProgress <32645904+thesisInProgress@users.noreply.github.com>
2017-10-16 10:57:34 +0200

    Add files via upload

---
+++ check2.py
@@ -0,0 +1,66 @@
+import atexit
+
+import sys
+from pyVim.connect import SmartConnectNoSSL, Disconnect
+import pyVmomi
+from pyVmomi import vim
+import inspect
+
+
+
+from tools import cli
+
+import argparse
+import getpass
+import ssl
+
+from pyVim import connect
+
+def setup_args():
+
+    """
+    Get standard connection arguments
+    """
+    parser = cli.build_arg_parser()
+    my_args = parser.parse_args()
+
+    return cli.prompt_for_password(my_args)
+
+def main():
+    """
+    Simple command-line program for listing the virtual machines on a host.
+    """
+
+
+    args = setup_args()
+    si = None
+    try:
+        si = connect.ConnectNoSSL(host=args.host,
+                               user=args.user,
+                               pwd=args.password,
+                               port=int(args.port))
+        atexit.register(Disconnect, si)
+    except vim.fault.InvalidLogin:
+        raise SystemExit("Unable to connect to host "
+                         "with supplied credentials.")
+
+    content = si.RetrieveContent()
+#    print(vim.ServiceInstance.RetrieveContent(si))
+
+    ###############################
+    #Get inf about current user session
+    #privilege: readOnly
+    ###############################
+    print("##########################################")
+    print("Get info about current user session")
+    print("##########################################")
+    session = content.sessionManager.currentSession
+    print(session)
+
+    ###############################
+    #Get info about VMs
+    ###############################
+
+
+if __name__ == "__main__":
+    main()
